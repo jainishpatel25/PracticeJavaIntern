0f060bd386ef91244032619c5bda0ae06d54bbd7
jainish.patel <jainish.patel@CS82-PC>
2026-02-17 17:51:40 +0530

    "Treeset Example"

--- src/collectionexample/DescendingComparator.java
+++ src/collectionexample/DescendingComparator.java
@@ -7,7 +7,10 @@ class DescendingComparator implements Comparator<Integer>{
 	@Override
 	public int compare(Integer i1 , Integer i2) {
 		// TODO Auto-generated method stub
-		return i2.compareTo(i1);
+//		return i2.compareTo(i1);
+//		return +1;   //in insertion order
+//		return -1;   //reverse of insertion order
+		return 0;    //first print other all are duplicate
 	}
 
 	
--- src/collectionexample/TreeSetComparator.java
+++ src/collectionexample/TreeSetComparator.java
@@ -1,5 +1,6 @@
 package collectionexample;
 
+import java.util.Queue;
 import java.util.TreeSet;
 
 public class TreeSetComparator {
@@ -7,8 +8,8 @@ public class TreeSetComparator {
 	
 	public static void main(String[] args) {
 		
-		TreeSet<Integer> num = new TreeSet<>(new DescendingComparator());
-		
+		TreeSet<Integer> num = new TreeSet<>(new DescendingComparator());		
+
 		num.add(12);
 		num.add(52);
 		
@@ -17,5 +18,25 @@ public class TreeSetComparator {
 		num.add(32);
 		
 		System.out.println(num);
+//		System.out.println(num.ceiling(25));
+
+		
+//		TreeSet<StringBuilder> t = new TreeSet<>();
+//		
+//		t.add(new StringBuilder("L"));
+//		t.add(new StringBuilder("E"));
+//		t.add(new StringBuilder("R"));
+//		t.add(new StringBuilder("K"));
+//		t.add(new StringBuffer("C") );
+//		t.add(new StringBuffer("L"));
+//		t.add("B");
+//		t.add("B");
+//		t.add("B");
+		
+		
+		
+//		System.out.println(t);
+		
+ 		
 	}
 }
